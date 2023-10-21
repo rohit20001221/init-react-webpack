d39b25c2bb39383a351237b33c519d83c2c9b858
rohit20001221 <rohit20001221@gmail.com>
2023-10-21 12:44:26 +0530

    cli

--- bin/init-react-webpack.js
+++ bin/init-react-webpack.js
@@ -1,9 +1,30 @@
 #! /usr/bin/env node
 const fs = require("fs");
 const path = require("path");
+const { chdir } = require("process");
+const { execSync } = require("child_process");
 
-console.log("[*] init create react app with webpack");
-console.log("[*] reading react webpack template");
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-const templateDir = path.join(__dirname, "templates", "react-webpack");
-fs.cpSync(templateDir, "react-app", { recursive: true });
+readline.question("what is the name of your app?", async (appName) => {
+  console.log("[*] init create react app with webpack");
+  console.log("[*] reading react webpack template");
+
+  const templateDir = path.join(__dirname, "templates", "react-webpack");
+  fs.cpSync(templateDir, appName, { recursive: true });
+
+  chdir(appName);
+
+  fs.writeFileSync(".gitignore", "node_modules/\ndist/");
+
+  const packageJson = JSON.parse(fs.readFileSync("package.json").toString());
+  packageJson.name = appName;
+
+  fs.writeFileSync("package.json", JSON.stringify(packageJson));
+
+  execSync("git init");
+  execSync("npm install");
+});
