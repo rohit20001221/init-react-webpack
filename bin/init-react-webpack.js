b68e85178821cc9d23fe30bcf7f0d1f6030a839a
rohit20001221 <rohit20001221@gmail.com>
2023-10-21 12:34:38 +0530

    version 2

--- bin/init-react-webpack.js
+++ bin/init-react-webpack.js
@@ -5,8 +5,5 @@ const path = require("path");
 console.log("[*] init create react app with webpack");
 console.log("[*] reading react webpack template");
 
-const templateDir = fs.readdirSync(
-  path.join(__dirname, "templates", "react-webpack")
-);
-
+const templateDir = path.join(__dirname, "templates", "react-webpack");
 fs.cpSync(templateDir, "react-app", { recursive: true });
